c71f374129da42bc8e7cc012eae068d5a6718216
dbcas-dev <kg.dbpi@gmail.com>
2026-03-19 12:36:17 +0800

    Update MainUI.tsx

--- app/components/MainUI.tsx
+++ app/components/MainUI.tsx
@@ -31,7 +31,7 @@ function VerificationContent() {
     if (loading) {
       document.title = "Searching...";
     } else if (selectedRecord) {
-      document.title = `${record.photo_code} | ${record.album_name} - Capture and Share - Digital Image Sharing`;
+      document.title = `Verified: ${selectedRecord.photo_code} | DBCAS`;
     } else if (results.length > 1) {
       document.title = `Results for "${searchQuery}"`;
     } else {
